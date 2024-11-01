aebef4a8d39f840a30e5452b84eb2cb6ea9c567d
Matthias Benquet <matthiasbenquet@gmail.com>
2024-11-02 00:20:46 +0100

    CPU initialisation and some helper functions

--- include/cpu.h
+++ include/cpu.h
@@ -1,9 +1,58 @@
 #ifndef CPU_H
 #define CPU_H
 
+#include <stdint.h>
+#include <stdbool.h>
 
+#define MEM_SIZE 0x10000
 
+typedef struct flags {
+    uint8_t z:1;
+    uint8_t s:1;
+    uint8_t p:1;
+    uint8_t cy:1;
+    uint8_t ac:1;
+    uint8_t padding:3;
+} flags;
 
+typedef struct CPU {
+    uint8_t a;
+    uint8_t b;
+    uint8_t c;
+    uint8_t d;
+    uint8_t e;
+    uint8_t h;
+    uint8_t l;
+
+    flags f;
+    
+    uint16_t sp;
+    uint16_t pc;
+
+    uint8_t memory[MEM_SIZE];
+
+    bool exit;
+} CPU;
+
+void cpu_init(CPU *cpu);
+
+bool cpu_load_rom_at(CPU *cpu, char const *filename, uint16_t start);
+
+void cpu_tick(CPU *cpu);
+
+void clear_flags(CPU *cpu);
+
+uint8_t read_byte(CPU *cpu, uint16_t address);
+
+void write_byte(CPU *cpu, uint16_t address, uint8_t value);
+
+uint16_t read_word(CPU *cpu, uint16_t address);
+
+void write_word(CPU *cpu, uint16_t address, uint16_t value);
+
+uint8_t next_byte(CPU *cpu);
+
+uint16_t next_word(CPU *cpu);
 
 
 
--- src/cpu.c
+++ src/cpu.c
@@ -1,1 +1,74 @@
+#include <stdio.h>
+
 #include "cpu.h"
+
+
+void cpu_init(CPU *cpu) {
+    cpu->exit = false;
+
+    cpu->a = cpu->b = cpu->c = cpu->d = cpu->e = cpu->h = cpu->l = 0;
+
+    cpu->pc = cpu->sp = 0;
+
+    clear_flags(cpu);
+
+    for(int i = 0; i <= MEM_SIZE; ++i) {
+        cpu->memory[i] = 0;
+    }
+}
+
+bool cpu_load_rom_at(CPU *cpu, char const *filename, uint16_t start) {
+    FILE *f = fopen(filename, "rb");
+    if (f == NULL) {
+        return false;
+    }
+
+    fseek(f, 0L, SEEK_END);
+    int fsize = ftell(f);
+    fseek(f, 0L, SEEK_SET);
+
+    uint8_t *buffer = cpu->memory + start;
+    fread(buffer, fsize, 1, f);
+    fclose(f);
+
+    return true;
+}
+
+void cpu_tick(CPU *cpu) {
+    uint8_t opcode = cpu->memory[cpu->pc++];
+
+    switch(opcode) {
+    default:
+        break;
+    }
+}
+
+void clear_flags(CPU *cpu) {
+    cpu->f.ac = cpu->f.cy = cpu->f.p = cpu->f.s = cpu->f.z = 0;
+}
+
+uint8_t read_byte(CPU *cpu, uint16_t address) {
+    return cpu->memory[address];
+}
+
+void write_byte(CPU *cpu, uint16_t address, uint8_t value) {
+    cpu->memory[address] = value;
+}
+
+uint16_t read_word(CPU *cpu, uint16_t address) {
+    return cpu->memory[address + 1] << 8 | cpu->memory[address];
+}
+
+void write_word(CPU *cpu, uint16_t address, uint16_t value) {
+    cpu->memory[address] = value & 0xff;
+    cpu->memory[address + 1] = value >> 8;
+}
+
+uint8_t next_byte(CPU *cpu) {
+    return read_byte(cpu, cpu->pc++);
+}
+
+uint16_t next_word(CPU *cpu) {
+    cpu->pc += 2;
+    return read_word(cpu, cpu->pc - 2);
+}
